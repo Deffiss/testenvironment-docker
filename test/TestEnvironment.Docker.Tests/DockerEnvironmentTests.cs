3790b78054f0a12f03edc4150232a630904f29e9
Aliaksei Harshkalep <deffiss@gmail.com>
2018-09-30 15:54:29 +0300

    Print mssql and elastic versions.

--- test/TestEnvironment.Docker.Tests/DockerEnvironmentTests.cs
+++ test/TestEnvironment.Docker.Tests/DockerEnvironmentTests.cs
@@ -1,3 +1,7 @@
+using Nest;
+using System;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Threading.Tasks;
 using TestEnvironment.Docker.Containers;
 using Xunit;
@@ -21,13 +25,36 @@ namespace TestEnvironment.Docker.Tests
 
             // Play with containers.
             var mssql = environment.GetContainer<MssqlContainer>("my-mssql");
+            await PrintMssqlVersion(mssql);
+
             var elastic = environment.GetContainer<ElasticsearchContainer>("my-elastic");
+            await PrintElasticsearchVersion(elastic);
 
             // Down it.
             await environment.Down();
 
             // Dispose (remove).
             environment.Dispose();
+        }
+
+        private static async Task PrintMssqlVersion(MssqlContainer mssql)
+        {
+            using (var command = new SqlCommand("SELECT @@VERSION", new SqlConnection(mssql.GetConnectionString())))
+            {
+                command.Connection.Open();
+
+                var reader = await command.ExecuteReaderAsync();
+                await reader.ReadAsync();
+
+                Console.WriteLine($"MSSQL Version: {reader.GetString(0)}");
+            }
+        }
+
+        private static async Task PrintElasticsearchVersion(ElasticsearchContainer elastic)
+        {
+            var elasticClient = new ElasticClient(new Uri(elastic.GetUrl()));
+            var clusterInfo = await elasticClient.NodesInfoAsync();
+            Console.WriteLine($"Elasticsearch version: {clusterInfo.Nodes.Values.First().Version}");
         }
     }
 }
