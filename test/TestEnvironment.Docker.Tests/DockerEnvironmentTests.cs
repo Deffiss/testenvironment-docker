ea0ed3efcb7efe73e95da173c997abbfb0e52fee
Kiryl_Kiryanchykau <kiryl_kiryanchykau@epam.com>
2019-09-30 17:36:45 +0300

    feature/Adding-postgres-container
    Add postgres container

--- test/TestEnvironment.Docker.Tests/DockerEnvironmentTests.cs
+++ test/TestEnvironment.Docker.Tests/DockerEnvironmentTests.cs
@@ -12,10 +12,12 @@ using TestEnvironment.Docker.Containers.MariaDB;
 using TestEnvironment.Docker.Containers.Mail;
 using TestEnvironment.Docker.Containers.Mongo;
 using TestEnvironment.Docker.Containers.Mssql;
+using TestEnvironment.Docker.Containers.Postgres;
 using Xunit;
 using MySql.Data.MySqlClient;
 using FluentFTP;
 using MailKit.Net.Smtp;
+using Npgsql;
 
 namespace TestEnvironment.Docker.Tests
 {
@@ -33,10 +35,11 @@ namespace TestEnvironment.Docker.Tests
                 .AddElasticsearchContainer("my-elastic", ports: new Dictionary<ushort, ushort> { [9200] = 9200 }, reuseContainer: true)
                 .AddMssqlContainer("my-mssql", "HelloK11tt_0", environmentVariables: new Dictionary<string, string> { ["MSSQL_COLLATION"] = "SQL_Latin1_General_CP1_CS_AS" }, reuseContainer: true)
                 .AddMariaDBContainer("my-maria", "my-secret-pw", reuseContainer: true)
-                .AddMongoContainer("my-mongo", reuseContainer: true)
+                .AddMongoContainer("my-mongo", reuseContainer: true)                
                 .AddMailContainer("my-mail", reuseContainer: true)
                 .AddFtpContainer("my-ftp", "superuser", "test", ports: Enumerable.Range(30000, 10).ToDictionary(p => (ushort)p, p => (ushort)p).MergeDictionaries(new Dictionary<ushort, ushort> { [21] = 21 }), reuseContainer: true)
                 .AddFromDockerfile("from-file", "Dockerfile", containerWaiter: new HttpContainerWaiter("/", httpPort: 8080), reuseContainer: true)
+                .AddPostgresContainer("my-postgres", reuseContainer: true)
 #else
                 .AddElasticsearchContainer("my-elastic")
                 .AddMssqlContainer("my-mssql", "HelloK11tt_0")
@@ -45,6 +48,7 @@ namespace TestEnvironment.Docker.Tests
                 .AddMailContainer("my-mail")
                 .AddFromDockerfile("from-file", "Dockerfile", containerWaiter: new HttpContainerWaiter("/", httpPort: 8080))
                 .AddFtpContainer("my-ftp", "superuser", "test", ports: Enumerable.Range(30000, 10).ToDictionary(p => (ushort)p, p => (ushort)p).MergeDictionaries(new Dictionary<ushort, ushort> { [21] = 21 }))
+                .AddPostgresContainer("my-postgres")
 
 #endif
                 .Build();
@@ -73,6 +77,9 @@ namespace TestEnvironment.Docker.Tests
 
             var mail = environment.GetContainer<MailContainer>("my-mail");
             await PrintSmtpCapabilities(mail);
+
+            var postgres = environment.GetContainer<PostgresContainer>("my-postgres");
+            await PrintPostgresDbVersion(postgres);
 
 #if !DEBUG
             // Down it.
@@ -154,6 +161,20 @@ namespace TestEnvironment.Docker.Tests
             {
                 var response = await client.GetStringAsync("/");
                 Console.WriteLine($"Response from static server: {response}");
+            }
+        }
+
+        private static async Task PrintPostgresDbVersion(PostgresContainer postgres)
+        {
+            using (var connection = new NpgsqlConnection(postgres.GetConnectionString()))
+            using (var command = new NpgsqlCommand("select version();", connection))
+            {
+                await connection.OpenAsync();
+
+                var reader = await command.ExecuteReaderAsync();
+                await reader.ReadAsync();
+
+                Console.WriteLine($"Postgres Version: {reader.GetString(0)}");
             }
         }
     }
