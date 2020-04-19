0ee2d0e65433bfc46e2cef81923db014b7111b89
Vadim Hatsura <vadim.hatsura@gmail.com>
2020-04-19 22:03:23 +0300

    revert unrelated changes

--- src/TestEnvironment.Docker.Containers.Elasticsearch/ElasticsearchContainer.cs
+++ src/TestEnvironment.Docker.Containers.Elasticsearch/ElasticsearchContainer.cs
@@ -6,14 +6,10 @@ namespace TestEnvironment.Docker.Containers.Elasticsearch
 {
     public class ElasticsearchContainer : Container
     {
-        public ElasticsearchContainer(DockerClient dockerClient, string name,
-            string imageName = "docker.elastic.co/elasticsearch/elasticsearch-oss", string tag = "7.0.1",
-            IDictionary<string, string> environmentVariables = null, IDictionary<ushort, ushort> ports = null,
-            bool isDockerInDocker = false, bool reuseContainer = false, ILogger logger = null)
+        public ElasticsearchContainer(DockerClient dockerClient, string name, string imageName = "docker.elastic.co/elasticsearch/elasticsearch-oss", string tag = "7.0.1", IDictionary<string, string> environmentVariables = null, IDictionary<ushort, ushort> ports = null, bool isDockerInDocker = false, bool reuseContainer = false, ILogger logger = null)
             : base(dockerClient, name, imageName, tag,
-                new Dictionary<string, string> {["discovery.type"] = "single-node"}.MergeDictionaries(
-                    environmentVariables), ports, isDockerInDocker, reuseContainer,
-                new ElasticsearchContainerWaiter(logger), new ElasticsearchContainerCleaner(), logger)
+                new Dictionary<string, string> { ["discovery.type"] = "single-node" }.MergeDictionaries(environmentVariables), ports,
+                isDockerInDocker, reuseContainer, new ElasticsearchContainerWaiter(logger), new ElasticsearchContainerCleaner(), logger)
         {
         }
 
