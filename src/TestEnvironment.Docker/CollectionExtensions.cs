572e8b1179a3034fdf5625bb9a74ba0c57b7746e
Joseph Woodward <josephwoodward@xeuse.com>
2021-05-12 12:15:21 +0100

    Fix private port clash between IPv4 and IPv6 on MacOS (#53)

---
+++ src/TestEnvironment.Docker/CollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestEnvironment.Docker
+{
+    internal static class CollectionExtensions
+    {
+        internal static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            var seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
+    }
+}
